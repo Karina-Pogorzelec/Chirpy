3258c1b65dfac6253d6b0be936c559ca907b89e0
Karina-Pogorzelec <kk.pogorzelec@gmail.com>
2026-02-22 16:55:18 +0100

    20260222

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Karina-Pogorzelec/Chirpy
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main() {
+	serverMux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: serverMux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
