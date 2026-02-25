29e036e50c41d731ebe14ddd6eb25c301fe0b37a
Karina-Pogorzelec <kk.pogorzelec@gmail.com>
2026-02-25 10:20:30 +0100

    20260225

--- main.go
+++ main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"net/http"
 	"log"
+	"sync/atomic"
+	"fmt"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+
 	serverMux := http.NewServeMux()
+
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
+	serverMux.HandleFunc("GET /api/healthz", handlerReadiness)
+
+	serverMux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+
+	serverMux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
@@ -16,4 +33,27 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d\n", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
